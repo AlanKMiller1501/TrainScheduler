e712bf34944dd0ab5ca62fd461def5d23da53e1b
Alan32575 <52725927+Alan32575@users.noreply.github.com>
2019-10-22 18:08:23 -0400

    more java

--- java.js
+++ java.js
@@ -14,5 +14,33 @@ var database = firebase.database();
 //build a capture button and prevent default
 $("addTrain").on("click", function (event)
  {event.preventDefault ();
-    //build my variables
-}
+    //build my variables 
+    var trainName = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#firstTrain").val().trim();
+    var freq = $("#interval").val().trim();
+    
+
+  database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: freq
+    });
+  });
+ // create a temporary object for holding the new train data
+ var newTrn = {
+  name: trnName,
+  destination: trnDest,
+  firstTime: firstTrnTime,
+  frequency: trnFreq
+};
+
+// upload the new train data to the database
+database.ref().push(newTrn);
+
+// console log the values that were just pushed to the database
+console.log(newTrn.name);
+console.log(newTrn.destination);
+console.log(newTrn.firstTime);
+ s
